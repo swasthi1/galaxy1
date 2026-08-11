6eda2a4f985058578ae32d05519c8fb7a1e94b0b
swasthi1 <swasthivelan12@gmail.com>
2026-08-11 14:13:20 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import flask
 app=Flask(__name__)
 @app.route("/")
 def name():
-    return "Helloworld ! ,This is my second app render
+    return "helloworld ! ,This is my second app render
 if __name__=="__main":
     app.run(host="0.0.0.0",port=5000)
     
